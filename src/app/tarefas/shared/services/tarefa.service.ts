fa7d5d2e55791ad275aa2965bb1c8a1e14ce771d
Sávio Bertoldi <77998135+saviuu@users.noreply.github.com>
2022-12-22 10:16:34 -0300

    Implementando service de tarefas

--- src/app/tarefas/shared/services/tarefa.service.ts
+++ src/app/tarefas/shared/services/tarefa.service.ts
@@ -12,4 +12,26 @@ export class TarefaService {
     const tarefas = localStorage['tarefas'];
     return tarefas ? JSON.parse(tarefas) : [];
   }
+
+  cadastrar(tarefa: TarefaModel): void {
+    const tarefas = this.listarTodas();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  buscaPorId(id: number): TarefaModel {
+    const tarefas: TarefaModel[] = this.listarTodas();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizar(tarefa: TarefaModel):void {
+    const tarefas: TarefaModel[] = this.listarTodas();
+    tarefas.forEach((obj, index, objs) => {
+      if(tarefa.id === obj.id){
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
 }
