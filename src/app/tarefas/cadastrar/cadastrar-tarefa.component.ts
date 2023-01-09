3065334eaaa20655aafb5202c6c90ae7e9e149d5
Sávio Bertoldi <77998135+saviuu@users.noreply.github.com>
2023-01-09 18:43:47 -0300

    Implementando componente de cadastro + html

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { TarefaService } from './../shared/services/tarefa.service';
+import { TarefaModel } from './../shared/models/tarefa.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
   templateUrl: './cadastrar-tarefa.component.html',
   styleUrls: ['./cadastrar-tarefa.component.css']
 })
-export class CadastrarTarefaComponent {
+export class CadastrarTarefaComponent implements OnInit {
 
+  @ViewChild('formTarefa') formTarefa: NgForm;
+  tarefa: TarefaModel;
+
+  constructor(
+    private _tarefaService: TarefaService,
+    private _router: Router
+    ){}
+
+  ngOnInit() {
+    this.tarefa = new TarefaModel();
+  }
+
+  cadastrar(): void {
+    if(this.formTarefa.form.valid){
+      this._tarefaService.cadastrar(this.tarefa);
+      this._router.navigate(["/tarefas"]);
+    }
+  }
 }
